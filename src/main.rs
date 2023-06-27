d067bf6167261ca36554cf2569348a84b0e1ab53
Steve Korber <korbersa@outlook.com>
2023-06-27 19:38:46 -0400

    Got Hello, World! to flash to the board

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello World");
+    let mut count = 0;
+    loop {
+        rprintln!("loop {}", count);
+        count += 1;
+    }
+}
