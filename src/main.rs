75b2079f3505f5a8946410cdf43ad1cfc3055a51
Steve Korber <korbersa@outlook.com>
2023-06-28 21:32:25 -0400

    Blinking a single LED is working

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,28 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use microbit as _;
+use microbit::{
+    board::Board,
+    hal::prelude::*,
+    hal::timer::Timer,
+};
 use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    // Variable setup
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
-    // infinite loop; just so we don't leave this stack frame
+    // Main loop with counter
     let mut i = 0;
+    board.display_pins.col1.set_low().unwrap();
     loop {
         hprintln!("cycle {}", i);
+        board.display_pins.row1.set_high().unwrap();
+        timer.delay_ms(1000u16);
+        board.display_pins.row1.set_low().unwrap();
+        timer.delay_ms(1000u16);
         i += 1;
     }
 }
