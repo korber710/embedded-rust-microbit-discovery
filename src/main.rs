7eeca2f3a21241200450f2c405882b2e7525c221
Steve Korber <korbersa@outlook.com>
2023-06-29 21:40:15 -0400

    THe roulette wheel is working, it could still get more optimization, there is redundancy in the function

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,77 @@ use cortex_m_rt::entry;
 use panic_halt as _;
 use microbit::{
     board::Board,
-    hal::prelude::*,
-    hal::timer::Timer,
+    pac::TIMER0,
+    hal::{prelude::*, timer::Timer},
+    display::blocking::Display,
 };
 use cortex_m_semihosting::hprintln;
+
+fn increment_row(row: &mut usize, col: usize, direction: bool, display: &mut Display, timer: &mut Timer<TIMER0>) {
+    let empty: [[u8; 5]; 5] = [
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+    ];
+    if *row < 5 {
+        for _ in 0..4 {
+            let mut array = empty.clone();
+            array[*row][col] = 1;
+            display.show(timer, array, 100u32);
+            // display.clear();
+            drop(array);
+            if direction {
+                *row += 1;
+            } else {
+                *row -= 1;
+            }
+        }
+    }
+}
+
+fn increment_col(row: usize, col: &mut usize, direction: bool, display: &mut Display, timer: &mut Timer<TIMER0>) {
+    let empty: [[u8; 5]; 5] = [
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0,],
+    ];
+    if *col < 5 {
+        for _ in 0..4 {
+            let mut array = empty.clone();
+            array[row][*col] = 1;
+            display.show(timer, array, 100u32);
+            // display.clear();
+            drop(array);
+            if direction {
+                *col += 1;
+            } else {
+                *col -= 1;
+            }
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
     // Variable setup
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
     // Main loop with counter
     let mut i = 0;
-    board.display_pins.col1.set_low().unwrap();
+    let mut col = 0;
+    let mut row = 0;
     loop {
-        hprintln!("cycle {}", i);
-        board.display_pins.row1.set_high().unwrap();
-        timer.delay_ms(1000u16);
-        board.display_pins.row1.set_low().unwrap();
-        timer.delay_ms(1000u16);
+        // hprintln!("cycle {}", i);
+        increment_row(&mut row, col, true, &mut display, &mut timer);
+        increment_col(row, &mut col, true, &mut display, &mut timer);
+        increment_row(&mut row, col, false, &mut display, &mut timer);
+        increment_col(row, &mut col, false, &mut display, &mut timer);
         i += 1;
     }
 }
