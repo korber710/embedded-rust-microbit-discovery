49220653deddfab3abc476715a4b0559322a98f8
Steve Korber <steve.korber@molex.com>
2023-07-10 08:20:42 -0400

    sending a single byte to UART is working

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,34 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use microbit::{
-    board::Board,
-    pac::TIMER0,
-    hal::{prelude::*, timer::Timer},
-    display::blocking::Display,
-};
-use cortex_m_semihosting::hprintln;
 
-fn increment_led(
-    row: &mut usize,
-    col: &mut usize,
-    use_row: bool,
-    increase: bool,
-    display: &mut Display,
-    timer: &mut Timer<TIMER0>)
-{
-    let empty: [[u8; 5]; 5] = [
-        [0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0,],
-    ];
-    if *row < 5 && *col < 5 {
-        for _ in 0..4 {
-            let mut array = empty.clone();
-            array[*row][*col] = 1;
-            display.show(timer, array, 30u32);
-            // display.clear();
-            drop(array);
-            if increase {
-                if use_row {
-                    *row += 1;
-                } else {
-                    *col += 1;
-                }
-            } else {
-                if use_row {
-                    *row -= 1;
-                } else {
-                    *col -= 1;
-                }
-            }
-        }
-    }
-}
+use microbit::{
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+};
+
+mod serial_setup;
+use serial_setup::UartePort;
 
 #[entry]
 fn main() -> ! {
-    // Variable setup
-    let board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
-    let mut display = Display::new(board.display_pins);
+    let board = microbit::Board::take().unwrap();
 
-    // Main loop with counter
-    let mut i = 0;
-    let mut col = 0;
-    let mut row = 0;
-    loop {
-        // hprintln!("cycle {}", i);
-        let increment_upwards = true;
-        let increment_downwards = false;
-        let update_row = true;
-        let update_col = false;
-        increment_led(&mut row, &mut col, update_row, increment_upwards, &mut display, &mut timer);
-        increment_led(&mut row, &mut col, update_col, increment_upwards, &mut display, &mut timer);
-        increment_led(&mut row, &mut col, update_row, increment_downwards, &mut display, &mut timer);
-        increment_led(&mut row, &mut col, update_col, increment_downwards, &mut display, &mut timer);
-        i += 1;
-    }
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    nb::block!(serial.write(b'X')).unwrap();
+    nb::block!(serial.flush()).unwrap();
+
+    loop {}
 }
